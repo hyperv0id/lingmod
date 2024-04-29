191f75dc965789b84d9e2c20aad1b7b7a8d9390b
Echo <2385961813@qq.com>
2024-04-29 13:38:16 +0800

    卡牌修改：梦为鱼鸟。现在可以在牌库中永久移除了

--- src/main/java/lingmod/cards/MengWeiYuNiao.java
+++ src/main/java/lingmod/cards/MengWeiYuNiao.java
@@ -1,26 +1,34 @@
 package lingmod.cards;
 
 import basemod.ReflectionHacks;
+import basemod.helpers.CardModifierManager;
 import com.megacrit.cardcrawl.actions.common.DamageAction;
 import com.megacrit.cardcrawl.cards.DamageInfo;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import lingmod.cards.mod.DreamMod;
 import lingmod.util.MonsterHelper;
-import lingmod.util.TODO;
 
 import static lingmod.ModCore.makeID;
 
+/**
+ * 梦为鱼鸟：造成怪物的伤害
+ * 你梦中变成鸟便振翅直飞蓝天，你梦中变成鱼便摇尾潜入深渊
+ */
 public class MengWeiYuNiao extends AbstractEasyCard {
-
 
     public static final String ID = makeID(MengWeiYuNiao.class.getSimpleName());
 
-
     public MengWeiYuNiao() {
         super(ID, 1, CardType.ATTACK, CardRarity.SPECIAL, CardTarget.ENEMY);
-        this.exhaust = true;
-        baseDamage = 7;
+        purgeOnUse = true;
+        CardModifierManager.addModifier(this, new DreamMod(1));
+    }
+
+    @Override
+    public boolean canUpgrade() {
+        return false;
     }
 
     @Override
@@ -30,21 +38,23 @@ public class MengWeiYuNiao extends AbstractEasyCard {
 
     @Override
     public void use(AbstractPlayer abstractPlayer, AbstractMonster abstractMonster) {
-        int totalDamage = 0;
-
+        int totalDamage = calc_dmg();
+        addToBot(new DamageAction(abstractMonster, new DamageInfo(abstractPlayer, totalDamage, DamageInfo.DamageType.NORMAL)));
+    }
+    protected int calc_dmg() {
+        int total = 0;
         for (AbstractMonster mo : AbstractDungeon.getCurrRoom().monsters.monsters) {
             if (!mo.isDeadOrEscaped()) {
                 mo.createIntent();
                 if (!MonsterHelper.isAttackIntent(mo)) continue;
                 int moDamage = ReflectionHacks.getPrivate(mo, AbstractMonster.class, "intentDmg");
-                if (moDamage == -1) continue;
+                if (moDamage <= 0) continue;
                 if (ReflectionHacks.getPrivate(mo, AbstractMonster.class, "isMultiDmg")) {
                     moDamage *= (int) ReflectionHacks.getPrivate(mo, AbstractMonster.class, "intentMultiAmt");
                 }
-                totalDamage += moDamage;
+                total+= moDamage;
             }
         }
-        addToBot(new DamageAction(abstractMonster, new DamageInfo(abstractPlayer, totalDamage, DamageInfo.DamageType.NORMAL)));
-        TODO.info("永久移除 MengWeiYuNiao");
+        return total;
     }
 }
---
+++ src/main/java/lingmod/cards/mod/DreamMod.java
@@ -0,0 +1,49 @@
+package lingmod.cards.mod;
+
+import basemod.abstracts.AbstractCardModifier;
+import basemod.helpers.CardModifierManager;
+import com.evacipated.cardcrawl.mod.stslib.StSLib;
+import com.megacrit.cardcrawl.actions.utility.UseCardAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.core.AbstractCreature;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import lingmod.ModCore;
+
+public class DreamMod extends  AbstractCardModifier{
+    protected int limit = 1;
+    protected boolean masterCardRemoved = false;
+
+    public DreamMod(int limit) {
+        super();
+        this.limit = limit;
+    }
+
+    @Override
+    public void onUse(AbstractCard card, AbstractCreature target, UseCardAction action) {
+        ModCore.logger.info("MengMod: onUse Card func");
+        --this.limit;
+        card.initializeDescription();
+        AbstractCard masterCard = StSLib.getMasterDeckEquivalent(card);
+        if (masterCard != null) {
+            DreamMod dreamMod = null;
+
+            for(AbstractCardModifier mod : CardModifierManager.getModifiers(masterCard, this.identifier(null))) {
+                if (mod instanceof DreamMod) {
+                    dreamMod = (DreamMod) mod;
+                    --dreamMod.limit;
+                }
+            }
+
+            masterCard.initializeDescription();
+            if (dreamMod != null && dreamMod.limit == 0) {
+                AbstractDungeon.player.masterDeck.removeCard(masterCard);
+                this.masterCardRemoved = true;
+            }
+        }
+    }
+
+    @Override
+    public AbstractCardModifier makeCopy() {
+        return new DreamMod(limit);
+    }
+}
