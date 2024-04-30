b44bf01a2e4d9c11a7dcbea2939c4dae6f4fbfc9
Echo <2385961813@qq.com>
2024-04-30 12:22:38 +0800

    添加初始遗物图像

---
+++ src/main/java/lingmod/cards/ZuiFeiChen.java
@@ -0,0 +1,30 @@
+package lingmod.cards;
+
+import basemod.helpers.CardModifierManager;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import lingmod.cards.mod.AppendDescMod;
+
+import static lingmod.ModCore.makeID;
+
+public class ZuiFeiChen extends AbstractEasyCard{
+
+    public final static String ID = makeID(ZuiFeiChen.class.getSimpleName());
+    public String lastCardDesc = null;
+    public ZuiFeiChen() {
+        super(ID, 1, CardType.ATTACK, CardRarity.BASIC, CardTarget.ENEMY);
+        this.exhaust = true;
+        this.baseMagicNumber = 1;
+        this.initializeDescription();
+        CardModifierManager.addModifier(this, new AppendDescMod());
+    }
+    @Override
+    public void upp() {
+
+    }
+
+    @Override
+    public void use(AbstractPlayer abstractPlayer, AbstractMonster abstractMonster) {
+
+    }
+}
