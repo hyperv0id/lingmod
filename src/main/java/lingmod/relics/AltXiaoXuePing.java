2a037aa169960f47374ace45f42090200a62b31b
jcheng <20722003@bjtu.edu.cn>
2024-05-05 14:13:36 +0800

    小血瓶：胜利时的格挡可用于下一场战斗

---
+++ src/main/java/lingmod/relics/AltXiaoXuePing.java
@@ -0,0 +1,36 @@
+package lingmod.relics;
+
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.actions.common.GainBlockAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.powers.watcher.FreeAttackPower;
+
+import static lingmod.ModCore.makeID;
+
+/**
+ * 小血瓶：格挡可用于下一场战斗
+ */
+public class AltXiaoXuePing extends AbstractEasyRelic {
+    public static final String ID = makeID(AltXiaoXuePing.class.getSimpleName());
+
+    public AltXiaoXuePing() {
+        super(ID, RelicTier.SPECIAL, LandingSound.FLAT);
+    }
+
+    @Override
+    public void onVictory() {
+        super.onVictory();
+        AbstractPlayer player = AbstractDungeon.player;
+        this.counter = player.currentBlock;
+    }
+
+    @Override
+    public void atBattleStart() {
+        super.atBattleStart();
+        addToBot(new GainBlockAction(AbstractDungeon.player, this.counter));
+        this.counter = 0;
+    }
+}
