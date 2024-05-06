37a44b78bc891af289e29ee428660682a33132f8
jcheng <20722003@bjtu.edu.cn>
2024-05-07 00:43:43 +0800

    添加卡牌铁斩波ALT和清平

---
+++ src/main/java/lingmod/cards/AltTieZhanBo.java
@@ -0,0 +1,38 @@
+package lingmod.cards;
+
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.actions.common.DamageAction;
+import com.megacrit.cardcrawl.cards.DamageInfo;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.powers.PlatedArmorPower;
+import lingmod.potions.DemoPotion;
+
+import static lingmod.ModCore.makeID;
+
+public class AltTieZhanBo extends AbstractEasyCard {
+    public final static String ID = makeID(AltTieZhanBo.class.getSimpleName());
+
+    public final static int BASE_DAMAGE = 5; // 造成伤害
+    public final static int BASE_DAMAGE_P = 2;
+    public final static int PA_NUM = 4; // 多层护甲
+    public final static int PA_NUM_P = 1;
+
+
+    public AltTieZhanBo() {
+        super(ID, 1, CardType.ATTACK, CardRarity.COMMON, CardTarget.ENEMY);
+        baseDamage = BASE_DAMAGE;
+        baseMagicNumber = PA_NUM;
+    }
+
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        addToBot(new DamageAction(m, new DamageInfo(p, damage, DamageInfo.DamageType.NORMAL)));
+        addToBot(new ApplyPowerAction(p, p, new PlatedArmorPower(p, baseMagicNumber)));
+    }
+
+    @Override
+    public void upp() {
+        upgradeBlock(3);upgradeDamage(BASE_DAMAGE_P);
+        upgradeMagicNumber(PA_NUM_P);
+    }
+}
---
+++ src/main/java/lingmod/cards/Tranquility.java
@@ -0,0 +1,44 @@
+package lingmod.cards;
+
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.actions.common.DamageAction;
+import com.megacrit.cardcrawl.cards.DamageInfo;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.powers.StrengthPower;
+import lingmod.powers.AltDemonPower;
+
+import static lingmod.ModCore.makeID;
+
+/**
+ * 清平：打5，下次增加3点,升级后打8增加5点。是fibonacci数列
+ */
+public class Tranquility extends AbstractEasyCard{
+    public static final String ID = makeID(Tranquility.class.getSimpleName());
+
+    public static final int COST = 1;
+
+    public static final int F1 = 1;
+    public static final int F2 = 1;
+    public static final int F3 = 2;
+
+
+    public Tranquility() {
+        super(ID, COST, CardType.ATTACK, CardRarity.COMMON, CardTarget.ENEMY);
+        this.baseDamage = F3;
+        this.baseSecondDamage = F2;
+    }
+
+    @Override
+    public void use(AbstractPlayer player, AbstractMonster monster) {
+        addToBot(new DamageAction(monster, new DamageInfo(player, damage, DamageInfo.DamageType.NORMAL)));
+        int t = baseDamage;
+        baseDamage += baseSecondDamage;
+        baseSecondDamage = t;
+    }
+    @Override
+    public void upp() {
+        upgradeDamage(F2);
+        upgradeSecondDamage(F1);
+    }
+}
