d61ffbf732b70828990eb3d61fe19d03b8ce6e35
jcheng <20722003@bjtu.edu.cn>
2024-06-05 15:14:17 +0800

    重置孤独卡牌效果

--- src/main/java/lingmod/cards/curse/Lonely.java
+++ src/main/java/lingmod/cards/curse/Lonely.java
@@ -1,55 +1,44 @@
 package lingmod.cards.curse;
 
-import com.megacrit.cardcrawl.cards.AbstractCard;
-import com.megacrit.cardcrawl.characters.AbstractPlayer;
-import com.megacrit.cardcrawl.monsters.AbstractMonster;
-import lingmod.cards.AbstractEasyCard;
-
 import static lingmod.ModCore.makeID;
 
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+
+import lingmod.cards.AbstractEasyCard;
+
 /**
- * 孤独：可被打出。在手牌中时，打出的牌耗能++
+ * 孤独：可被打出。在手牌中时，未打出的牌耗能++
  */
-public class Lonely extends AbstractEasyCard{
+public class Lonely extends AbstractEasyCard {
 
     public static final String ID = makeID(Lonely.class.getSimpleName());
-    public boolean trigger = false;
-    
+
     public Lonely() {
         super(ID, 1, CardType.CURSE, CardRarity.SPECIAL, CardTarget.ALL);
         dontTriggerOnUseCard = true;
     }
 
     @Override
-    public void triggerWhenDrawn() {
-        super.triggerWhenDrawn();
-        trigger = true;
-    }
-
-    @Override
-    public void onMoveToDiscard() {
-        super.onMoveToDiscard();
-        trigger = false;
-    }
-
-    @Override
-    public void onPlayCard(AbstractCard c, AbstractMonster m) {
-        super.onPlayCard(c, m);
-        if(!trigger) return;
-        c.cost++;
-        c.isCostModified = true;
-    }
-
-    @Override
-    public boolean canUpgrade() {
-        return false;
+    public void triggerOnEndOfPlayerTurn() {
+        // TODO Auto-generated method stub
+        super.triggerOnEndOfPlayerTurn();
+        for (AbstractCard card : AbstractDungeon.player.hand.group) {
+            card.cost++;
+            card.costForTurn++;
+            card.isCostModified = true;
+            card.isCostModifiedForTurn = true;
+        }
     }
 
     @Override
     public void upp() {
+        updateCost(-1);
     }
 
     @Override
     public void use(AbstractPlayer arg0, AbstractMonster arg1) {
-    }    
+    }
 }
