f933201dbd71c668981d82e60dc02b4e0058905f
jcheng <20722003@bjtu.edu.cn>
2024-06-05 12:40:04 +0800

    一个礼物：添加特效

--- src/main/java/lingmod/cards/skill/AGiftCard.java
+++ src/main/java/lingmod/cards/skill/AGiftCard.java
@@ -1,18 +1,22 @@
 package lingmod.cards.skill;
 
-import basemod.cardmods.ExhaustMod;
-import basemod.helpers.CardModifierManager;
+import static lingmod.ModCore.makeID;
+
 import com.megacrit.cardcrawl.actions.AbstractGameAction.AttackEffect;
+import com.megacrit.cardcrawl.actions.animations.VFXAction;
 import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.core.Settings;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
 import com.megacrit.cardcrawl.powers.VulnerablePower;
 import com.megacrit.cardcrawl.powers.WeakPower;
+import com.megacrit.cardcrawl.vfx.combat.ShockWaveEffect;
+
+import basemod.cardmods.ExhaustMod;
+import basemod.helpers.CardModifierManager;
 import lingmod.cards.AbstractEasyCard;
 import lingmod.cards.mod.NellaFantasiaMod;
-
-import static lingmod.ModCore.makeID;
 
 public class AGiftCard extends AbstractEasyCard {
 
@@ -28,13 +32,14 @@ public class AGiftCard extends AbstractEasyCard {
 
     @Override
     public void use(AbstractPlayer p, AbstractMonster m) {
+        addToBot(new VFXAction(p, new ShockWaveEffect(p.hb.cX, p.hb.cY, Settings.BLUE_TEXT_COLOR, ShockWaveEffect.ShockWaveType.ADDITIVE), 0.5F));
         for (AbstractMonster mo : AbstractDungeon.getCurrRoom().monsters.monsters) {
-            this.addToBot(new ApplyPowerAction(mo, p, new WeakPower(mo, this.magicNumber, false), this.magicNumber,
-                    true, AttackEffect.NONE));
             this.addToBot(new ApplyPowerAction(mo, p, new VulnerablePower(mo, this.magicNumber, false),
                     this.magicNumber, true, AttackEffect.NONE));
+            this.addToBot(new ApplyPowerAction(mo, p, new WeakPower(mo, this.magicNumber, false), this.magicNumber,
+                    true, AttackEffect.NONE));
         }
-        // addToBot(new ApplyPowerAction(p, p, new VulnerablePower(p, secondMagic, false)));
+        addToBot(new ApplyPowerAction(p, p, new VulnerablePower(p, secondMagic, false)));
         addToBot(new ApplyPowerAction(p, p, new WeakPower(p, secondMagic, false)));
     }
 
