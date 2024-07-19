8493c46960ec5177cdba5dc805e20d5d9f6747e3
jcheng <20722003@bjtu.edu.cn>
2024-07-19 23:01:48 +0800

    fix: 湖松酒生效错误（sb代码提示

--- src/main/java/lingmod/cards/skill/HuSongWineCard.java
+++ src/main/java/lingmod/cards/skill/HuSongWineCard.java
@@ -1,15 +1,16 @@
 package lingmod.cards.skill;
 
-import basemod.ReflectionHacks;
+import static lingmod.ModCore.makeID;
+
 import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
 import com.megacrit.cardcrawl.powers.WeakPower;
+
+import basemod.ReflectionHacks;
 import lingmod.cards.AbstractWineCard;
 import lingmod.interfaces.CardConfig;
 import lingmod.util.MonsterHelper;
-
-import static lingmod.ModCore.makeID;
 
 /**
  * 敌人多段攻击，攻击次数减少 20%
@@ -39,7 +40,7 @@ public class HuSongWineCard extends AbstractWineCard {
                 multi *= (int) ((100 - magicNumber) / 100.0);
                 ReflectionHacks.setPrivate(mo, AbstractMonster.class, "intentMultiAmt", multi);
             } else {
-                addToBot(new ApplyPowerAction(p, mo, new WeakPower(mo, 1, false)));
+                addToBot(new ApplyPowerAction(mo, p, new WeakPower(mo, 1, false)));
             }
         }
     }
