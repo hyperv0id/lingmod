a605fa567236a82d94b5286c098e91b8dbdaef26
jcheng <20722003@bjtu.edu.cn>
2024-08-06 16:15:17 +0800

    event: 龙泡泡夕事件

--- src/main/java/lingmod/events/Beans_Dusk.java
+++ src/main/java/lingmod/events/Beans_Dusk.java
@@ -14,29 +14,29 @@ import basemod.abstracts.events.PhasedEvent;
 import basemod.abstracts.events.phases.TextPhase;
 import lingmod.ModCore.ResourceType;
 import lingmod.interfaces.CampfireSleepEvent;
-import lingmod.relics.Beans_LingRelic;
+import lingmod.relics.Beans_DuskRelic;
 
 @CampfireSleepEvent
-public class Beans_Ling extends PhasedEvent {
+public class Beans_Dusk extends PhasedEvent {
 
-    public static final String ID = makeID(Beans_Ling.class.getSimpleName());
+    public static final String ID = makeID(Beans_Dusk.class.getSimpleName());
     private static final EventStrings eventStrings = CardCrawlGame.languagePack.getEventString(ID);
     private static final String[] DESCRIPTIONS = eventStrings.DESCRIPTIONS;
     private static final String[] OPTIONS = eventStrings.OPTIONS;
-    protected AbstractRelic relic = new Beans_LingRelic();
+    protected AbstractRelic relic = new Beans_DuskRelic();
     public static String NAME = eventStrings.NAME;
 
-    public Beans_Ling() {
-        super(ID, eventStrings.NAME, makeImagePath("Beans_Ling_1.png", ResourceType.EVENTS));
+    // TODO: 图中是年，需要PS
+    public static String IMG_PATH = makeImagePath("Beans_Dusk.png", ResourceType.EVENTS);
 
+    public Beans_Dusk() {
+        super(ID, eventStrings.NAME, IMG_PATH);
         registerPhase(Phases.BEGIN, new TextPhase(DESCRIPTIONS[0])
                 .addOption(OPTIONS[1] + relic.name, (i) -> {
                     AbstractDungeon.getCurrRoom().spawnRelicAndObtain((float) (Settings.WIDTH / 2),
                             (float) (Settings.HEIGHT / 2), relic);
-                    this.imageEventText.loadImage(makeImagePath("Beans_Ling_2.png", ResourceType.EVENTS));
                     transitionKey(Phases.CHOOSE_1);
                 }, relic).addOption(OPTIONS[2], (i) -> {
-                    this.imageEventText.loadImage(makeImagePath("Beans_Ling_2.png", ResourceType.EVENTS));
                     transitionKey(Phases.CHOOSE_2);
                 }));
 
@@ -45,16 +45,16 @@ public class Beans_Ling extends PhasedEvent {
                     AbstractDungeon.getCurrRoom().phase = AbstractRoom.RoomPhase.COMPLETE;
                     openMap();
                 }));
+
         registerPhase(Phases.CHOOSE_2, new TextPhase(DESCRIPTIONS[2])
                 .addOption(OPTIONS[0], (i) -> {
                     AbstractDungeon.getCurrRoom().phase = AbstractRoom.RoomPhase.COMPLETE;
                     openMap();
                 }));
-
         transitionKey(Phases.BEGIN);
     }
 
-    static enum Phases {
+    public enum Phases {
         BEGIN, CHOOSE_1, CHOOSE_2
     }
 }
