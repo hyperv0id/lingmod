1d9c74dae6b7425171e975c11b76a80498cd17a0
Echo <2385961813@qq.com>
2024-08-25 20:46:44 +0800

    feat: 卡图裁剪

--- src/main/java/lingmod/cards/skill/ShanHeYuanKuo.java
+++ src/main/java/lingmod/cards/skill/ShanHeYuanKuo.java
@@ -3,16 +3,17 @@ package lingmod.cards.skill;
 import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
-
 import lingmod.ModCore;
 import lingmod.cards.AbstractEasyCard;
 import lingmod.interfaces.CardConfig;
+import lingmod.interfaces.Credit;
 import lingmod.powers.ShanHeYuanKuoPower;
 
 /**
  * 本回合你每打出一张牌就抽一张牌
  */
 @CardConfig(magic = 1)
+@Credit(username = "下野宏铭", link = "主页限制")
 public class ShanHeYuanKuo extends AbstractEasyCard {
     public static final String ID = ModCore.makeID(ShanHeYuanKuo.class.getSimpleName());
 
