1b8f4cdff254797344e5c16c5f80e8359fd79b26
jcheng <20722003@bjtu.edu.cn>
2024-09-08 22:07:32 +0800

    修复灼伤被攻击两次的问题

--- src/main/java/lingmod/patch/MonsterPatch.java
+++ src/main/java/lingmod/patch/MonsterPatch.java
@@ -3,6 +3,7 @@ package lingmod.patch;
 import basemod.ReflectionHacks;
 import com.evacipated.cardcrawl.modthespire.lib.SpirePatch;
 import com.evacipated.cardcrawl.modthespire.lib.SpirePostfixPatch;
+import com.evacipated.cardcrawl.modthespire.lib.SpireReturn;
 import com.megacrit.cardcrawl.actions.AbstractGameAction;
 import com.megacrit.cardcrawl.actions.common.DamageAction;
 import com.megacrit.cardcrawl.actions.common.GainBlockAction;
@@ -13,14 +14,11 @@ import com.megacrit.cardcrawl.core.AbstractCreature;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
 import com.megacrit.cardcrawl.monsters.MonsterGroup;
-import com.megacrit.cardcrawl.powers.PoisonPower;
-import com.megacrit.cardcrawl.powers.VulnerablePower;
-import com.megacrit.cardcrawl.powers.WeakPower;
+import com.megacrit.cardcrawl.relics.HandDrill;
 import lingmod.monsters.AbsSummonMonster;
 import lingmod.util.MonsterHelper;
 import lingmod.util.Wiz;
 
-import java.util.ArrayList;
 import java.util.Objects;
 
 import static lingmod.ModCore.logger;
@@ -31,14 +29,7 @@ public class MonsterPatch {
      * "https://steamcommunity.com/sharedfiles/filedetails/?id=2672531653">KaltsitMod</a>
      */
     public static class MonsterTakeDamagePatch {
-        private static final ArrayList<String> acceptDebuffs = new ArrayList<>();
         protected static AbstractCreature summonTarget;
-
-        static {
-            acceptDebuffs.add(WeakPower.POWER_ID);
-            acceptDebuffs.add(VulnerablePower.POWER_ID);
-            acceptDebuffs.add(PoisonPower.POWER_ID);
-        }
 
         /**
          * 获得召唤物，在角色被攻击时，角色替代受到伤害
@@ -69,8 +60,8 @@ public class MonsterPatch {
                 if (!Wiz.isPlayerLing())
                     return; // 其他角色无效
                 if (_inst.target == AbstractDungeon.player && Objects.equals(
-                        (Float) ReflectionHacks.getPrivate(_inst, AbstractGameAction.class, "duration"),
-                        (Float) ReflectionHacks.getPrivate(_inst, AbstractGameAction.class,
+                        ReflectionHacks.getPrivate(_inst, AbstractGameAction.class, "duration"),
+                        ReflectionHacks.getPrivate(_inst, AbstractGameAction.class,
                                 "startDuration"))
                         && MonsterTakeDamagePatch.gotSummon()) {
                     _inst.target = MonsterTakeDamagePatch.summonTarget;
@@ -80,48 +71,33 @@ public class MonsterPatch {
         }
 
         /**
-         * 施加能力效果转移到怪物上
-         */
-        // @SpirePatch(
-        // clz = ApplyPowerAction.class,
-        // method = "<ctor>",
-        // paramtypez = {AbstractCreature.class, AbstractCreature.class,
-        // AbstractPower.class, int.class, boolean.class,
-        // AbstractGameAction.AttackEffect.class}
-        // )
-        // public static class ChangeApplyBuffTarget {
-        // public static void Postfix(ApplyPowerAction _inst, AbstractCreature target,
-        // AbstractCreature source, AbstractPower power, int n, boolean b,
-        // AbstractGameAction.AttackEffect e) {
-        // if (!Wiz.isPlayerLing()) return; // 其他角色无效
-        // if (_inst != null && (source == null || !source.isPlayer) && target != null
-        // && target != source && (target == AbstractDungeon.player || power.owner ==
-        // AbstractDungeon.player) &&
-        // MonsterTakeDamagePatch.acceptDebuffs.contains(power.ID) &&
-        // MonsterTakeDamagePatch.gotSummon()) {
-        // _inst.target = MonsterTakeDamagePatch.summonTarget;
-        // power.owner = _inst.target;
-        // logger.info("本给予 " + target.name + " 的能力给予能力改变了目标。目前：target:" +
-        // _inst.target.name + ",owner:" + power.owner);
-        // }
-        // }
-        // }
-
-        /**
          * 闪退服了
          */
         @SpirePatch(clz = Burn.class, method = "use")
         public static class BurnPatch {
-            public static void Prefix(Burn _inst, AbstractPlayer p, AbstractMonster m) {
-                if (!Wiz.isPlayerLing())
-                    return; // 其他角色无效
+            public static SpireReturn<Void> Prefix(Burn _inst, AbstractPlayer p, AbstractMonster m) {
                 if (_inst.dontTriggerOnUseCard) {
                     AbstractDungeon.actionManager.addToBottom(new DamageAction(AbstractDungeon.player,
-                            new DamageInfo((AbstractCreature) null, _inst.magicNumber, DamageInfo.DamageType.THORNS),
+                            new DamageInfo(null, _inst.magicNumber, DamageInfo.DamageType.THORNS),
                             AbstractGameAction.AttackEffect.FIRE));
                 }
+                return SpireReturn.Return(null);
             }
         }
+
+        @SpirePatch(
+                clz = HandDrill.class,
+                method = "onBlockBroken"
+        )
+        public static class HandDrillPatch {
+            public HandDrillPatch() {
+            }
+
+            public static SpireReturn<Void> Prefix(HandDrill _inst, AbstractCreature c) {
+                return c instanceof AbsSummonMonster ? SpireReturn.Return(null) : SpireReturn.Continue();
+            }
+        }
+
 
         @SpirePatch(clz = MonsterGroup.class, method = "areMonstersDead")
         public static class EndBattleCheckPatch {
@@ -131,21 +107,13 @@ public class MonsterPatch {
             }
         }
 
-        @SpirePatch(clz = AbstractGameAction.class, method = "setValues", paramtypez = { AbstractCreature.class,
-                DamageInfo.class })
+        @SpirePatch(clz = AbstractGameAction.class, method = "setValues", paramtypez = {AbstractCreature.class,
+                DamageInfo.class})
         public static class ChangeDamageTarget {
             public static void Postfix(AbstractGameAction _inst, AbstractCreature target, DamageInfo info) {
-                if (!Wiz.isPlayerLing())
-                    return; // 其他角色无效
                 if (info == null)
                     return;
                 // 取消友伤
-                if (target != null && info.owner != null && info.owner.isPlayer && target.isPlayer
-                        && info.owner != target) {
-                    // _inst.target = MonsterHelper.getMoNotSummon(true, null);
-                    // _inst.isDone = true; // 取消这个
-                    // logger.info("友伤取消: " + info.owner + " " + target);
-                }
                 if (target != null && info.type != DamageInfo.DamageType.HP_LOSS
                         && (info.owner == null || !info.owner.isPlayer) && target == AbstractDungeon.player
                         && MonsterTakeDamagePatch.gotSummon()) {
